6be8a4ce77082816ed5e5705eb63d6a4cceb8ec4
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2024-03-17 16:16:28 +0300

    init

--- playground/urls.py
+++ playground/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('hello/', views.sayhello)
+    path('hello/', views.sayhello),
+    path('create/', views.create_my_model),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Person
+from django.http import JsonResponse
+import json
 # Create your views here.
-def sayhello(request):
+def htm(request):
     return render(request,'hello.html')
+
+
+def sayhello(request):
+    # return render(request,'hello.html')
+    my_model = Person.objects.all()
+    data = [{'name': model.name, 'description': model.description} for model in my_model]
+    return JsonResponse(data, safe=False)
+
+
+def create_my_model(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = data.get('name', '')
+        description = data.get('description', '')
+        my_model = Person.objects.create(name=name, description=description)
+        return JsonResponse({'id': my_model.id, 'name': my_model.name, 'description': my_model.description}, status=201)
+  
+    else:
+        return JsonResponse({"error":'methosnotallorw'},status=405)
